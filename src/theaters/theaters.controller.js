f90e98c3131fe18582d819473d05875c4f38de89
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-01-07 12:56:37 -0700

    Get request to route "/theaters" now returns with all movies and their information that are playing at each theater.

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,8 +1,21 @@
 const theatersService = require("./theaters.service");
+const reduceProperties = require("../utils/reduce-properties");
+
+const moviesConfig = {
+  movie_id: ["movies", null, "movie_id"],
+  title: ["movies", null, "title"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  rating: ["movies", null, "rating"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+  is_showing: ["movies", null, "is_showing"],
+};
+
+const reduceMovies = reduceProperties("theater_id", moviesConfig);
 
 async function list(req, res, next) {
   const theaters = await theatersService.list();
-  res.json({ theaters });
+  res.json({ data: reduceMovies(theaters) });
 }
 
 module.exports = {
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,10 @@
 const knex = require("../db/connection");
 
 function list() {
-  return knex("theaters").select("*");
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .select("*");
 }
 
 module.exports = {
