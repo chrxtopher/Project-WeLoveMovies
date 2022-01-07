9d9cd2d76b98391f69c1a2e8ce495269b4c6d287
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-01-07 12:13:27 -0700

    Get requests to "/theaters" returns all information for theaters in the database.

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,3 +1,10 @@
 const theatersService = require("./theaters.service");
 
-module.exports = {};
+async function list(req, res, next) {
+  const theaters = await theatersService.list();
+  res.json({ theaters });
+}
+
+module.exports = {
+  list,
+};
